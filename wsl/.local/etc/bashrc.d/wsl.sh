87a9c0a8c8850baeb607597ade1ddddc1ad21d9b
Ben Burrill <bburrill98@gmail.com>
2023-07-19 04:39:05 -0700

    WSL config

---
+++ wsl/.local/etc/bashrc.d/wsl.sh
@@ -0,0 +1,3 @@
+alias wps='powershell.exe'
+alias wpst='wt.exe -w 0 nt -p "Windows PowerShell" -d .'
+alias wpss='wt.exe -w 0 sp -p "Windows PowerShell" -d .'
---
+++ wsl/.local/etc/profile.d/wsl.sh
@@ -0,0 +1,1 @@
+"$HOME/.local/bin/wsl-ssh-agent-relay" start && export SSH_AUTH_SOCK="$HOME/.ssh/wsl-ssh-agent.sock"
