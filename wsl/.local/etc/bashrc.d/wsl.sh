2e97108d0fbdbef3876ec8f92000e087ca1bf616
Ben Burrill <bburrill98@gmail.com>
2025-04-15 23:53:44 -0700

    Add a few more things to wsl.sh from local.sh

--- wsl/.local/etc/bashrc.d/wsl.sh
+++ wsl/.local/etc/bashrc.d/wsl.sh
@@ -1,3 +1,12 @@
 alias wps='powershell.exe'
 alias wpst='wt.exe -w 0 nt -p "Windows PowerShell" -d .'
 alias wpss='wt.exe -w 0 sp -p "Windows PowerShell" -d .'
+
+# sud'oh - run commands as root without the pesky inconvenience of
+# needing to enter a password.
+alias sudoh='wsl.exe -u root'
+alias fixclock='sudoh hwclock -s'
+function checkclock {
+    echo "Win: $(powershell.exe -Command Date -UFormat %c)"
+    echo "WSL: $(date +%c)"
+}
